f305450236b87aac0867f877d0f8cd0acf639565
lions-language <lions_language@163.com>
2021-04-17 22:51:51 +0800

    update

--- array/src/fixed/mod.rs
+++ array/src/fixed/mod.rs
@@ -1,17 +1,23 @@
 struct FixedArray<T> {
-    datas: Vec<T>
+    datas: Vec<T>,
+    index: 0,
+    size: u32
 }
 
 impl<T> FixedArray<T> {
-    fn push(&mut self) {
+    fn push(&mut self, t: T) {
+        // if atomic.
     }
 
     fn pop(&mut self) -> Option<T> {
         unimplemented!();
     }
 
-    pub fn new() -> Self {
+    pub fn new(size: u32) -> Self {
         Self {
+            datas: Vec::new(),
+            index: 0,
+            size: size
         }
     }
 }
