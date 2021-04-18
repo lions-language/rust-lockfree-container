aa76afa0fee70c5b66627910d687c2765fbe3c83
lions-language <lions_language@163.com>
2021-04-18 17:19:49 +0800

    update push interface

--- array/src/fixed/mod.rs
+++ array/src/fixed/mod.rs
@@ -1,12 +1,27 @@
+use std::sync::atomic::{AtomicU32, Ordering};
+
 struct FixedArray<T> {
     datas: Vec<T>,
-    index: 0,
-    size: u32
+    index: AtomicU32,
+    size: u32,
+    last: u32
 }
 
 impl<T> FixedArray<T> {
-    fn push(&mut self, t: T) {
-        // if atomic.
+    fn push(&mut self, t: T) -> bool {
+        let mut v = self.index.compare_and_swap(self.size, self.last, Ordering::Relaxed);
+        if v == self.size {
+            return false;
+        } else {
+            /*
+             * > size
+             * */
+            v = self.index.compare_and_swap(self.last, self.last, Ordering::Relaxed);
+            if v == self.last {
+                return false;
+            }
+        }
+        return true;
     }
 
     fn pop(&mut self) -> Option<T> {
@@ -15,9 +30,10 @@ impl<T> FixedArray<T> {
 
     pub fn new(size: u32) -> Self {
         Self {
-            datas: Vec::new(),
-            index: 0,
-            size: size
+            datas: vec![T; size],
+            index: AtomicU32::new(0),
+            size: size,
+            last: size+1
         }
     }
 }
